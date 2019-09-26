adbdb77a41384b749768189b916b71cde7781def
RaviPrakashMP <54932206+RaviPrakashMP@users.noreply.github.com>
2019-09-26 15:42:35 +0530

    Update S02Q01_mileage_odometer readings.py

--- S02Q01_mileage_odometer readings.py
+++ S02Q01_mileage_odometer readings.py
@@ -4,6 +4,6 @@ NOD = float(input("Please enter the new odometer reading"))
 
 GallonsofGasUsed = float(input("Please enter the gallons og gas used"))
 
-MilesPerGallon = NOD-OOD/GallonsofGasUsed
+MilesPerGallon = (NOD-OOD)/GallonsofGasUsed
 
 print( "The car's miles-per-gallon is" + str(MilesPerGallon) + "miles/gallon" )
